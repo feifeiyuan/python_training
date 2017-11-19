a8fd0eb1ba30815152e501bfa5080057d43f0a94
feifeiyuan <2094757536@qq.com>
2017-11-19 18:21:29 -0500

    add content to string

--- test_list1.py
+++ test_list1.py
@@ -13,3 +13,23 @@ a,b = 0,1
 while b<10:
 	print(b,end=',')
 	a, b = b, a+b # 复合赋值运算会在变动之前执行
+print()
+
+# for python3 six basic data type Numbers
+#type
+a,b,c,d = 20, 45.9, True, 4+7j
+print(type(a),type(b),type(c), type(d))
+#comp 
+print((a+b), (a*b), (c/d), (a//b), (d-a))
+
+# for python3 six basic data type String
+s = 'yes this is a string\t' # add r complment natual str
+print(s, type(s), len(s))
+# strcat
+bay="baby\n"
+print(bay+"I\t"+"like\t"*3+"you")
+# reverse
+t = "python"
+print(t[0], t[-1]) #p n
+print(t[0:3],t[2:3]) # pyt t
+# t[0] = 's' this is error
