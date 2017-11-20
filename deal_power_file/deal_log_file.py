b4cd23d93518bff30543f1ae31cedb970e9812f5
feifeiyuan <2094757536@qq.com>
2017-11-20 01:58:44 -0500

    解析log文件中的信息，并进行简单计算

--- deal_power_file/deal_log_file.py
+++ deal_power_file/deal_log_file.py
@@ -7,30 +7,39 @@ import re
 # 获取传入的参数
 import sys
 file_name = sys.argv[1]
-str1 = "d cap"
-
+str1 = "sprdfgu: d cap"
+str2 = "\d{2}-\d{2} \d{2}:\d{2}:\d{2}" # 解析日期
+str3 = "\d*mAh" # 电流值
+list_time = []
+list_value = []
 # try except
 try:
 	# 打开文件
-	file = open(file_name, encoding="utf-8", errors='ignore')
-
+	file_obj = open(file_name, "rt" , encoding="utf-8", errors='ignore')
 	# 循环读取文件
 	while True:
-		content = file.readline()
+		content = file_obj.readline()
 		if not content:
 			break
 
 		# 在字符串中查找包含指定字符串的字串,返回值为第一个字串的索引，未找到返回-1
-		index = conten.find(str1)
-		if index = -1:
+		index = content.find(str1)
+		if index != -1:
 			# 查找字符串中正则匹配的信息,返回值为一个list
 			index_time = re.findall(str2, content)
 			index_value = re.findall(str3, content)
+			
+			# 将list转换成字串
+			index_time = ''.join(index_time)
+			index_value = ''.join(index_value)
+
+
 			print(index_time)
 			print(index_value)
 			
-		
-
+			#将字符串压入list
+			list_time.append(index_time)
+			list_value.append(index_value)
 except:
 	print("the file is not exist")
 
